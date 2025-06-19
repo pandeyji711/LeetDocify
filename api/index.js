4de5e8d42f767ba0f24c42a89e5c434a692e64c8
Anurag Pandey <anuragpandey88699@gmail.com>
2025-06-19 16:15:00 +0530

    vercel

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-const serverless = require("serverless-http");
-const app = require("../app"); // this imports your existing Express app
-
-module.exports = app;
-module.exports.handler = serverless(app);
--- index.js
+++ index.js
@@ -63,9 +63,8 @@ app.post("/submit", async (req, res) => {
     );
   }
 });
-module.exports = app;
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () =>
-//   console.log(`Server running on http://localhost:${PORT}`)
-// );
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
